cd49310653d28f7d046d9b233c03868383e3bdc9
Calihrymn <43664297+Calihrymn@users.noreply.github.com>
2023-03-29 13:35:19 +1030

    29/03/23 development: added a few interactible functions, completing the first room.

--- README.md
+++ README.md
@@ -4,18 +4,28 @@
  |                                                                           |  
 -O---------------------------------------------------------------------------O-  
  |                                                                           |  
- |   ███╗░░░███╗░█████╗░████████╗░█████╗░██╗░░██╗██████╗░░█████╗░██╗░░██╗    |  
- |   ████╗░████║██╔══██╗╚══██╔══╝██╔══██╗██║░░██║██╔══██╗██╔══██╗╚██╗██╔╝    |  
- |   ██╔████╔██║███████║░░░██║░░░██║░░╚═╝███████║██████╦╝██║░░██║░╚███╔╝░    |  
- |   ██║╚██╔╝██║██╔══██║░░░██║░░░██║░░██╗██╔══██║██╔══██╗██║░░██║░██╔██╗░    |  
- |   ██║░╚═╝░██║██║░░██║░░░██║░░░╚█████╔╝██║░░██║██████╦╝╚█████╔╝██╔╝╚██╗    |  
- |   ╚═╝░░░░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝    |  
+ |   ███╗   ███╗  █████╗ ████████╗ █████╗ ██╗  ██╗ ██████╗  █████╗ ██╗  ██╗    |  
+ |   ████╗ ████║ ██╔══██╗╚══██╔══╝██╔══██╗██║  ██║ ██╔══██╗██╔══██╗╚██╗██╔╝    |  
+ |   ██╔████╔██║ ███████║   ██║   ██║  ╚═╝███████║ ██████╦╝██║  ██║ ╚███╔╝     |  
+ |   ██║╚██╔╝██║ ██╔══██║   ██║   ██║  ██╗██╔══██║ ██╔══██╗██║  ██║ ██╔██╗     |  
+ |   ██║ ╚═╝ ██║ ██║  ██║   ██║   ╚█████╔╝██║  ██║ ██████╦╝╚█████╔╝██╔╝╚██╗    |  
+ |   ╚═╝     ╚═╝ ╚═╝  ╚═╝   ╚═╝    ╚════╝ ╚═╝  ╚═╝ ╚═════╝  ╚════╝ ╚═╝  ╚═╝    |  
  |                                                                           |  
 -O---------------------------------------------------------------------------O-  
  |                                                                           |  
 ```
 > gleb dubinin 1107  
 > 2023 digital tech major project submission  
+
+## Before running:
+
+**This game has several dependencies.**
+**Install them with the following console commands:**
+```
+pip install random
+pip install time
+pip install datetime
+```
 
 ## Meta-game Print Syntax
 
@@ -46,8 +56,24 @@
   - if the object is an inventory expander (ie. a backpack or bag of some sort), the player will have the appropriate number of inventory slots added.
 
 
+## Time
+
+
+## Inventory Expanders
+
+* None: "hands", 2 items
+* Clothing: "pockets", 4 items
+* Backpack: "bag", 6 items
+* Duffel: "duffel", 8 items
+
 ## Changelog
 
 #### 22-03-23
 
 * Removed the Desk and Laptop from the player's bedroom; the only purpose for those objects was in case the player didn't check the phone to see that it hadn't changed overnight, but as the bedroom has been turned into more of a tutorial level for the game's interactions, the desk and laptop have been cut as interactible objects.
+
+#### 27-03-23
+
+* Added extra inventory expanders; a backpack in the entryway and a duffel bag in the parents bedroom
+* Containers are now treated much more like individual rooms, in that the player has to use specific commands to open and close them, and they cannot interact with the rest of the room while they are there. Previously, opening a container would have given information on what was inside it, and the player had to use any other command they wished (ie. use, grab, etc.) from the room directly; now, opening a container transports the player to the container, they can only interact with what is there, and they must close it if they wish to go back to the rest of the room.
+* `whereami`, or more simply, `where`, `location` or `loc` commands all tell the player where they currently are.
--- matchbox.py
+++ matchbox.py
@@ -1,17 +1,17 @@
 # banner
 print(20*"\n")
-print(" |                                                                           |")
-print("-O---------------------------------------------------------------------------O-")
-print(" |                                                                           |")
-print(" |   ███╗░░░███╗░█████╗░████████╗░█████╗░██╗░░██╗██████╗░░█████╗░██╗░░██╗    |")
-print(" |   ████╗░████║██╔══██╗╚══██╔══╝██╔══██╗██║░░██║██╔══██╗██╔══██╗╚██╗██╔╝    |")
-print(" |   ██╔████╔██║███████║░░░██║░░░██║░░╚═╝███████║██████╦╝██║░░██║░╚███╔╝░    |")
-print(" |   ██║╚██╔╝██║██╔══██║░░░██║░░░██║░░██╗██╔══██║██╔══██╗██║░░██║░██╔██╗░    |")
-print(" |   ██║░╚═╝░██║██║░░██║░░░██║░░░╚█████╔╝██║░░██║██████╦╝╚█████╔╝██╔╝╚██╗    |")
-print(" |   ╚═╝░░░░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝    |")
-print(" |                                                                           |")
-print("-O---------------------------------------------------------------------------O-")
-print(" |                                                                           |")
+print(" |                                                                              |")
+print("-O------------------------------------------------------------------------------O-")
+print(" |                                                                              |")
+print(" |    ███╗   ███╗  █████╗ ████████╗ █████╗ ██╗  ██╗ ██████╗  █████╗ ██╗  ██╗    |")
+print(" |    ████╗ ████║ ██╔══██╗╚══██╔══╝██╔══██╗██║  ██║ ██╔══██╗██╔══██╗╚██╗██╔╝    |")
+print(" |    ██╔████╔██║ ███████║   ██║   ██║  ╚═╝███████║ ██████╦╝██║  ██║ ╚███╔╝     |")
+print(" |    ██║╚██╔╝██║ ██╔══██║   ██║   ██║  ██╗██╔══██║ ██╔══██╗██║  ██║ ██╔██╗     |")
+print(" |    ██║ ╚═╝ ██║ ██║  ██║   ██║   ╚█████╔╝██║  ██║ ██████╦╝╚█████╔╝██╔╝╚██╗    |")
+print(" |    ╚═╝     ╚═╝ ╚═╝  ╚═╝   ╚═╝    ╚════╝ ╚═╝  ╚═╝ ╚═════╝  ╚════╝ ╚═╝  ╚═╝    |")
+print(" |                                                                              |")
+print("-O------------------------------------------------------------------------------O-")
+print(" |                                                                              |")
 print("\n\n\n\n\n")
 
 # imports
@@ -23,16 +23,21 @@ import math
 playing = False
 debugMode = False # dev mode skipping all the time delays
 noPower = False # what dictates when the player realises that there's no power
+hasWatch = False # does the player have the watch?
 usermove = "" # placeholder variable for all the input commands
 inputValid = False 
 location = "pregame" # the variable that dictates the game phase and the room the player is in
 locationint = 0
+container = "" #the name of the container the player is currently looking in (blank if they're in a room)
+room = "playersbedroom" # the name of the room the player's currently in - less precise than location or container
 
 locationsVisited = [] # variable that holds all the rooms the player has been to, to ensure that the room description doesn't play every time the player goes there
 
 inventory = []
+invcapacity = 2
+carrier = "hands"
 
-playersbedroom = ["closet//clothing", "lamp", "phone", "bedside table//watch", "bedside table//bedroom keys"] # all the items in the players bedroom
+playersbedroom = ["closet//clothing", "lamp", "phone", "bedside_table//watch", "bedside_table//bedroom_keys"] # all the items in the players bedroom
 
 playersbedroomDescription = ["You look around the bedroom you've lived in for as long as you can remember.",
                              "There'a window for you to admire the flames rapidly approaching your house,",
@@ -62,14 +67,48 @@ def wait(delay, debugMode):
     if not(debugMode):
         time.sleep(delay)
 
+def tellTheTime(time):
+    pass
+
 def printDescription(room, debugMode): # called when reading out the description of a room when it's entered for the first time or the "observe" command is used.
     for i in range(len(room)):
         print(room[i])
 
-        #█▀█ █▀▀ █▀▄▀█ █▀█ █░█ █▀▀   █▀▄▀█ █▀▀
-        #█▀▄ ██▄ █░▀░█ █▄█ ▀▄▀ ██▄   █░▀░█ ██▄
+        #█▀█ █▀▀ █▀▄▀█ █▀█ █ █ █▀▀   █▀▄▀█ █▀▀
+        #█▀▄ ██▄ █ ▀ █ █▄█ ▀▄▀ ██▄   █ ▀ █ ██▄
         if 1==2:
             wait(math.ceil(len(room[i]) / 35), debugMode)
+
+
+# functions for specific items:
+
+
+def usephone():
+    if noPower:
+        print("You already know there's no power, your phone doesn't work")
+    else:
+        noPower = True
+        print("You double tap on your phone screen")
+        wait(1, debugMode)
+        print("nothing.")
+        print("you press the power button.")
+        wait(1.5, debugMode)
+        print("nothing.")
+        print("You hold down the power button for a few seconds")
+        wait(2, debugMode)
+        print("1...")
+        wait(1, debugMode)
+        print("2...")
+        wait(1, debugMode)
+        print("3...")
+        wait(3, debugMode)
+        print("\n\n█████████████████████")
+        print("███░              ███")
+        print("███░              █████")
+        print("███░              ███")
+        print("█████████████████████\n\n")
+        wait(2, debugMode)
+        print("The power cut out overnight.")
 # goes through the arrays of messages for the designated room line by line, prints one, and waits for a certain amount of time
 # dependant on the length of the previous message before going to the next line.
 
@@ -78,8 +117,8 @@ def printDescription(room, debugMode): # called when reading out the description
 # pre-game intro section
 
 
-#█▀█ █▀▀ █▀▄▀█ █▀█ █░█ █▀▀   █▀▄▀█ █▀▀
-#█▀▄ ██▄ █░▀░█ █▄█ ▀▄▀ ██▄   █░▀░█ ██▄
+#█▀█ █▀▀ █▀▄▀█ █▀█ █ █ █▀▀   █▀▄▀█ █▀▀
+#█▀▄ ██▄ █ ▀ █ █▄█ ▀▄▀ ██▄   █ ▀ █ ██▄
 
 # wait(2, debugMode)
 
@@ -134,8 +173,8 @@ while(not(playing)): # intro menu
 while(playing):
  
 
-    #█▀█ █▀▀ █▀▄▀█ █▀█ █░█ █▀▀   █▀▄▀█ █▀▀
-    #█▀▄ ██▄ █░▀░█ █▄█ ▀▄▀ ██▄   █░▀░█ ██▄
+    #█▀█ █▀▀ █▀▄▀█ █▀█ █ █ █▀▀   █▀▄▀█ █▀▀
+    #█▀▄ ██▄ █ ▀ █ █▄█ ▀▄▀ ██▄   █ ▀ █ ██▄
 
     if location == "initialising" and 1==2:  # <- remove the 1==2
         # initialisation text that just describes the scene the first time the player's going thru it
@@ -200,30 +239,85 @@ while(playing):
                             print("you toggle the switch a couple more times, before settling with the fact that there's no power.")
 
                     if usermove == "phone":
-                        if noPower:
-                            print("You already know there's no power, your phone doesn't work")
+                        usephone()
+
+                    if usermove == "watch":
+                        print("you slide the watch straps around your wrist.")
+
+                elif container+"//"+usermove in house[locationint]:
+                    if usermove == "watch":
+                        print("you slide the watch on your wrist or whatever")
+
+    elif usermove.startswith("open "):
+        usermove = usermove.replace("open ", "")
+        #if usermove in (house[locationint]).split('/', 1)[0]:
+        #    container = usermove
+        #    location = room+"//"+usermove
+        for i in range(len(house[locationint])):
+            if usermove == house[locationint][i].split('/', 1)[0]:
+                container = usermove
+                location = room+"//"+usermove
+                print("You look inside the "+usermove)
+
+    elif usermove.startswith("grab "):
+
+        usermove = usermove.replace("grab ", "")
+
+        if usermove in house[locationint] or container+"//"+usermove in house[locationint]: # if the item is in the room or container the player is in
+            if "//" not in usermove:
+                if len(inventory)-1 < invcapacity:
+
+                    inventory.append(usermove) #add the item to the players inventory
+
+                    if usermove in house[locationint]: #remove the item from the room
+                        house[locationint].remove(usermove)
+                    elif container+"//"+usermove in house[locationint]:
+                        house[locationint].remove(container+"//"+usermove)
+
+                    print(house[locationint])
+
+                    if usermove == "watch":
+                        print("You slide the watch onto your wrist") 
+                        hasWatch = True
+                    else:
+                        if carrier == "hands":
+                            print("You take the "+usermove+". ")
                         else:
-                            print("You double tap on your phone screen")
-                            wait(1, debugMode)
-                            print("nothing.")
-                            print("you press the power button.")
-                            wait(1.5, debugMode)
-                            print("nothing.")
-                            print("You hold down the power button for a few seconds")
-                            wait(2, debugMode)
-                            print("1...")
-                            wait(1, debugMode)
-                            print("2...")
-                            wait(1, debugMode)
-                            print("3...")
-                            wait(3, debugMode)
-                            print("█████████████████████")
-                            print("███░              ███")
-                            print("███░              █████")
-                            print("███░              ███")
-                            print("█████████████████████\n\n")
-                            wait(2, debugMode)
-                            print("The power cut out overnight.")
+                            print("You put the "+usermove+" in your "+carrier+". ")
+                else:
+                    print("You're carrying too much to take the "+usermove+". ")
+            else:
+                print("What do you think you're trying to do? ") #if the player tries to take something that's in a container when they haven't opened it
+        else:
+            print("I couldn't find a "+usermove+" anywhere nearby. ") 
+
+    elif usermove.startswith("drop "):
+        usermove = usermove.replace("drop ", "")
+        if usermove in inventory:
+            if container == "":
+                house[locationint].append(usermove)
+                print("You drop the "+usermove)
+            else:
+                house[locationint].append(container+"//"+usermove)
+                print("You drop the "+usermove+" inside the "+container)
+            inventory.remove(usermove)
+        else:
+            print("You aren't carrying a "+usermove+". ")
+
+    elif usermove == "close":
+        container = ""
+        location = room                        
+        
+    elif usermove == "whereami" or usermove == "where" or usermove == "location" or usermove == "loc":
+        if container != "":
+            print("You are currently at "+room+", looking inside the "+container)
+        else:
+            print("You are currently at "+room) #fix this to display better versions of the location names
+
+    elif usermove == "inv" or usermove == "inventory":
+        print("At the moment, you're carrying: ")
+        for i in range(len(inventory)):
+            print(inventory[i-1])
 
     else:
 
